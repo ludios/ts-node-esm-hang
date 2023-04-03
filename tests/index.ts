c3ce0e4cd3a54b15e39e47643464602f9111303d
Ivan Kozik <ivan@ludios.org>
2023-04-03 08:30:51 +0000

    Minimize

--- tests/index.ts
+++ tests/index.ts
@@ -2,16 +2,6 @@ import test from "ava";
 import { A, AssertionError } from "../index.js";
 
 test('A', t => {
-	// number messages are supported despite not being validly typed
-	// @ts-expect-error
-	A(3, 3);
-	t.throws(() => A(false), {instanceOf: AssertionError, message: /^A\(\.\.\.\): false not truthy$/});
-	t.throws(() => A(false), {instanceOf: AssertionError, message: /^A\(\.\.\.\): false not truthy$/});
-	t.throws(() => A(0), {instanceOf: AssertionError, message: /^A\(\.\.\.\): 0 not truthy$/});
-	t.throws(() => A(""), {instanceOf: AssertionError, message: /^A\(\.\.\.\): '' not truthy$/});
-	t.throws(() => A(0), {instanceOf: AssertionError, message: "A(...): 0 not truthy"});
-	t.throws(() => A(0, "explanation"), {instanceOf: AssertionError, message: "A(...): 0 not truthy: explanation"});
-	t.throws(() => A(0, A.fn, () => "explanation"), {instanceOf: AssertionError, message: "A(...): 0 not truthy: explanation"});
 	// undefined return is supported despite not being validly typed
 	// @ts-expect-error
 	t.throws(() => A(0, A.fn, () => undefined), {instanceOf: AssertionError, message: "A(...): 0 not truthy: undefined"});

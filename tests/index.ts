48ded60e4d8940b6025f98241f947db496fd8122
Ivan Kozik <ivan@ludios.org>
2023-04-03 08:35:40 +0000

    Minimize

--- tests/index.ts
+++ tests/index.ts
@@ -1,21 +1,21 @@
 import test from "ava";
-import { A, AssertionError } from "../index.js";
+import { A } from "../index.js";
 
 test('A', t => {
 	// undefined return is supported despite not being validly typed
 	// @ts-expect-error
-	t.throws(() => A(0, A.fn, () => undefined), {instanceOf: AssertionError, message: "fail"});
+	t.throws(() => A(0, A.fn, () => undefined));
 	// number messages are supported despite not being validly typed
 	// @ts-expect-error
-	t.throws(() => A(0, 0), {instanceOf: AssertionError, message: "fail"});
+	t.throws(() => A(0, 0));
 });
 
 test('A.eq', t => {
 	A.eq(3, 3);
-	t.throws(() => A.eq(3, "3"), {instanceOf: AssertionError, message: "fail"});
+	t.throws(() => A.eq(3, "3"));
 });
 
 test('A.neq', t => {
 	A.neq(3, "3");
-	t.throws(() => A.neq(3, 3), {instanceOf: AssertionError, message: "fail"});
+	t.throws(() => A.neq(3, 3));
 });
